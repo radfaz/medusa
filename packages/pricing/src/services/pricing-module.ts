61d08b48986d100f76a8b1e55bd3bed17995c6d6
Philip Korsholm <philip.korsholm@hotmail.com>
2023-11-30 14:57:04 +0000

    add service types

--- packages/pricing/src/services/pricing-module.ts
+++ packages/pricing/src/services/pricing-module.ts
@@ -51,7 +51,7 @@ import {
   RuleTypeService,
 } from "@services"
 import { joinerConfig } from "../joiner-config"
-import { RepositoryTypes } from "../types"
+import { RepositoryTypes } from "@types"
 
 type InjectedDependencies = {
   baseRepository: DAL.RepositoryService
